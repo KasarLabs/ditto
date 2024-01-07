45782bf3f4b7e5d528e2c5fbf51d80115b1f40c1
Trantorian <eliot.code@proton.me>
2024-01-07 16:25:05 +0000

    test(rpc_test): :white_check_mark: invalid block test for getClassHashAt

---
+++ test/tests/test_get_class_hash_at.rs
@@ -0,0 +1,30 @@
+#![feature(assert_matches)]
+
+const STARKGATE_ETH_CONTRACT_ADDR: &str = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+
+use std::assert_matches::assert_matches;
+
+use rpc_test::test_config::TestConfig;
+use starknet::{providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider, ProviderError, StarknetErrorWithMessage, MaybeUnknownErrorCode}, core::types::{BlockId, FieldElement, StarknetError}};
+use url::Url;
+
+#[tokio::test]
+async fn fail_non_existing_block() {
+    let config = TestConfig::new("./secret.json").expect("Error loading test config");
+    let deoxys = JsonRpcClient::new(HttpTransport::new(
+        Url::parse(&config.deoxys).expect("Error parsing Deoxys api url")
+    ));
+
+    let response_deoxys = deoxys.get_class_hash_at(
+        BlockId::Hash(FieldElement::ZERO),
+        FieldElement::from_hex_be(STARKGATE_ETH_CONTRACT_ADDR).unwrap()
+    ).await.err();
+
+    assert_matches!(
+        response_deoxys,
+        Some(ProviderError::StarknetError(StarknetErrorWithMessage {
+            message: _,
+            code: MaybeUnknownErrorCode::Known(StarknetError::BlockNotFound)
+        }))
+    )
+}
