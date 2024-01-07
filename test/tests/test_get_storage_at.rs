bb7561c33846bd33ce6542f01963cb086514c9b0
Trantorian <eliot.code@proton.me>
2024-01-07 18:12:14 +0000

    test(rpc_test): :white_check_mark: invalid block test for getStorageAt

--- test/tests/test_get_storage_at.rs
+++ test/tests/test_get_storage_at.rs
@@ -10,6 +10,28 @@ use starknet::{providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider, Prov
 use url::Url;
 
 #[tokio::test]
+async fn fail_non_existing_block() {
+    let config = TestConfig::new("./secret.json").expect("Error loading tests config");
+    let deoxys = JsonRpcClient::new(HttpTransport::new(
+        Url::parse(&config.deoxys).expect("Error parsing Deoxys api url")
+    ));
+
+    let response_deoxys = deoxys.get_storage_at(
+        FieldElement::from_hex_be(CONTRACT_ADDR).unwrap(),
+        FieldElement::from_hex_be(CONTRACT_KEY).unwrap(),
+        BlockId::Hash(FieldElement::ZERO)
+    ).await.err();
+
+    assert_matches!(
+        response_deoxys,
+        Some(ProviderError::StarknetError(StarknetErrorWithMessage {
+            message: _,
+            code: MaybeUnknownErrorCode::Known(StarknetError::BlockNotFound)
+        }))
+    )
+}
+
+#[tokio::test]
 async fn work_get_storage() {
     let config = TestConfig::new("./secret.json").expect("Error loading tests config");
     let deoxys = JsonRpcClient::new(HttpTransport::new(
