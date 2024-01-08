09827fb5adab77c9a5e14ae817766cd80b932882
Trantorian <eliot.code@proton.me>
2024-01-08 15:28:55 +0000

    test(rpc_test): :white_check_mark: Valid test with args for starknet_call
    Call is made to StarkGate Eth bridge to retrieve the Eth balance of an account, passing that account as parameter.

--- unit_tests/src/constants.rs
+++ unit_tests/src/constants.rs
@@ -4,6 +4,13 @@ pub const DEOXYS: &str = "deoxys";
 pub const PATHFINDER: &str = "pathfinder";
 pub const STARKGATE_ETH_CONTRACT_ADDR: &str = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 pub const INVALID_CONTRACT_ADDR: &str = "0x4269DEADBEEF";
+
+/**
+ * Contract address for largest ETH holder as of time of development
+ * 
+ * Details concerning this specific contract can be found on StarkScan:
+ * https://starkscan.co/contract/0x03a20d4f7b4229e7c4863dab158b4d076d7f454b893d90a62011882dc4caca2a
+ */
 pub const CONTRACT_ADDR: &str = "0x03a20d4f7b4229e7c4863dab158b4d076d7f454b893d90a62011882dc4caca2a";
 pub const CONTRACT_KEY: &str = "0x00f920571b9f85bdd92a867cfdc73319d0f8836f0e69e06e4c5566b6203f75cc";
 
@@ -13,7 +20,7 @@ pub const CONTRACT_KEY: &str = "0x00f920571b9f85bdd92a867cfdc73319d0f8836f0e69e0
  * Details concerning available methods can be found on StarkScan:
  * https://starkscan.co/contract/0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7#read-write-contract
  */
-pub const STARKGATE_ETH_BRIDGE_ADDR: &str = "0x048624e084dc68d82076582219c7ed8cb0910c01746cca3cd72a28ecfe07e42d";
+pub const STARKGATE_ETH_BRIDGE_ADDR: &str = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
 pub const ACCOUNT_CONTRACT: &str = "";
 pub const TEST_CONTRACT_ADDRESS: &str = "";
--- unit_tests/tests/test_call.rs
+++ unit_tests/tests/test_call.rs
@@ -1,11 +1,11 @@
 #![feature(assert_matches)]
 
 mod common;
-use std::collections::HashMap;
+use std::{collections::HashMap, assert_matches::assert_matches};
 
 use common::*;
-use starknet_core::{types::{FunctionCall, BlockTag, BlockId, FieldElement}, utils::get_selector_from_name};
-use starknet_providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider};
+use starknet_core::{types::{FunctionCall, BlockTag, BlockId, FieldElement, StarknetError}, utils::get_selector_from_name};
+use starknet_providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider, ProviderError, StarknetErrorWithMessage, MaybeUnknownErrorCode};
 
 #[rstest]
 #[tokio::test]
@@ -31,5 +31,45 @@ async fn work_correct_call(clients: HashMap<String, JsonRpcClient<HttpTransport>
         BlockId::Tag(BlockTag::Latest)
     ).await.expect("Error waiting for response from Pathfinder node");
 
+    // hash of string value 'Ether'
+    let response_expected = FieldElement::from_hex_be("0x4574686572").unwrap();
+
+    assert_eq!(response_deoxys[0], response_expected);
+    assert_eq!(response_deoxys, response_pathfinder);
+}
+
+#[rstest]
+#[tokio::test]
+async fn work_correct_call_with_args(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
+    let deoxys = &clients[DEOXYS];
+    let pathfinder = &clients[PATHFINDER];
+
+    let response_deoxys = deoxys.call(
+        FunctionCall { 
+            contract_address: FieldElement::from_hex_be(STARKGATE_ETH_BRIDGE_ADDR).unwrap(), 
+            entry_point_selector: get_selector_from_name("balanceOf").unwrap(),
+            calldata: vec![
+                FieldElement::from_hex_be(CONTRACT_ADDR).unwrap()
+            ]
+        },
+        BlockId::Tag(BlockTag::Latest)
+    ).await.expect("Error waiting for response from Deoxys node");
+
+    let response_pathfinder = pathfinder.call(
+        FunctionCall { 
+            contract_address: FieldElement::from_hex_be(STARKGATE_ETH_BRIDGE_ADDR).unwrap(), 
+            entry_point_selector: get_selector_from_name("balanceOf").unwrap(),
+            calldata: vec![
+                FieldElement::from_hex_be(CONTRACT_ADDR).unwrap()
+            ]
+        },
+        BlockId::Tag(BlockTag::Latest)
+    ).await.expect("Error waiting for response from Pathfinder node");
+
+    let balance = u128::try_from(response_deoxys[0]).unwrap();
+
+    println!("balance: {balance}");
+
+    assert!(balance > 0);
     assert_eq!(response_deoxys, response_pathfinder);
 }
