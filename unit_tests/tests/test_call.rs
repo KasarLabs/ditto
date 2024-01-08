6d9b7b4ffa68cf23766bda5b3d4f3f68f49fd5fb
Trantorian <eliot.code@proton.me>
2024-01-08 15:56:49 +0000

    test(rpc_test): :white_check_mark: missing contract call data for starknet_call

--- unit_tests/tests/test_call.rs
+++ unit_tests/tests/test_call.rs
@@ -78,6 +78,29 @@ async fn fail_invalid_contract_entry_point_selector(clients: HashMap<String, Jso
 
 #[rstest]
 #[tokio::test]
+async fn fail_missing_contract_call_data(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
+    let deoxys = &clients[DEOXYS];
+
+    let response_deoxys = deoxys.call(
+        FunctionCall {
+            contract_address: FieldElement::from_hex_be(STARKGATE_ETH_BRIDGE_ADDR).unwrap(),
+            entry_point_selector: get_selector_from_name("balanceOf").unwrap(),
+            calldata: vec![]
+        },
+        BlockId::Tag(BlockTag::Latest)
+    ).await.err();
+
+    assert_matches!(
+        response_deoxys,
+        Some(ProviderError::StarknetError(StarknetErrorWithMessage {
+            message: _,
+            code: MaybeUnknownErrorCode::Known(StarknetError::ContractError)
+        }))
+    );
+}
+
+#[rstest]
+#[tokio::test]
 async fn work_correct_call(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
     let deoxys = &clients[DEOXYS];
     let pathfinder = &clients[PATHFINDER];
@@ -135,10 +158,8 @@ async fn work_correct_call_with_args(clients: HashMap<String, JsonRpcClient<Http
         BlockId::Tag(BlockTag::Latest)
     ).await.expect("Error waiting for response from Pathfinder node");
 
-    let balance = u128::try_from(response_deoxys[0]).unwrap();
+    let balance_deoxys = u128::try_from(response_deoxys[0]).unwrap();
 
-    println!("balance: {balance}");
-
-    assert!(balance > 0);
+    assert!(balance_deoxys > 0);
     assert_eq!(response_deoxys, response_pathfinder);
 }
