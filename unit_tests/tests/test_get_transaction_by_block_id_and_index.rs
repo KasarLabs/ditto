dfe975ae252de29579a509b9157663b286fa9351
Trantorian <eliot.code@proton.me>
2024-01-09 16:54:15 +0000

    test(get_transaction_by_block_id_and_index): :white_check_mark: getTransactionByBlockIdAndIndex L1_HANDLER

--- unit_tests/tests/test_get_transaction_by_block_id_and_index.rs
+++ unit_tests/tests/test_get_transaction_by_block_id_and_index.rs
@@ -42,6 +42,38 @@ async fn work_deploy_invoke(clients: HashMap<String, JsonRpcClient<HttpTransport
 ///
 /// Unit test for `starknet_getTransactionByBlockIdAndIndex`
 /// 
+/// purpose: get L1_HANDLER transaction.
+/// success case: client retrieves same transaction with getTransactionByBlockIdAndIndex and getTransactionByHash.
+/// 
+#[rstest]
+#[tokio::test]
+async fn work_deploy_l1_handler(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
+    let deoxys = &clients[DEOXYS];
+    let pathfinder = &clients[PATHFINDER];
+
+    // getting transaction through block number and index
+    let response_deoxys = deoxys.get_transaction_by_block_id_and_index(
+        BlockId::Number(TRANSACTION_L1_HANDLER_BLOCK_NB),
+        TRANSACTION_L1_HANDLER_INDEX
+    ).await.expect("Error waiting for response from Deoxys node");
+
+    let response_pathfinder = pathfinder.get_transaction_by_block_id_and_index(
+        BlockId::Number(TRANSACTION_L1_HANDLER_BLOCK_NB),
+        TRANSACTION_L1_HANDLER_INDEX
+    ).await.expect("Error waiting for response from Pathfinder node");
+
+    // getting transaction through hash
+    let response_expected = deoxys.get_transaction_by_hash(
+        FieldElement::from_hex_be(TRANSACTION_L1_HANDLER).unwrap()
+    ).await.expect("Error waiting for response from Deoxys node");
+
+    assert_eq!(response_deoxys, response_expected);
+    assert_eq!(response_deoxys, response_pathfinder);
+}
+
+///
+/// Unit test for `starknet_getTransactionByBlockIdAndIndex`
+/// 
 /// purpose: get DEPLOY_ACCOUNT transaction.
 /// success case: client retrieves same transaction with getTransactionByBlockIdAndIndex and getTransactionByHash.
 /// 
