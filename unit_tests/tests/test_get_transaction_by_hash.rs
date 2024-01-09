0780a26529df47a9a785caa461ec5872fd80b60d
Trantorian <eliot.code@proton.me>
2024-01-09 15:54:11 +0000

    docs(get_transaction_by_hash): :bulb: Commented tests for getTransacionByHash

--- unit_tests/tests/test_get_transaction_by_hash.rs
+++ unit_tests/tests/test_get_transaction_by_hash.rs
@@ -7,6 +7,12 @@ use common::*;
 use starknet_core::types::{FieldElement, StarknetError, Transaction};
 use starknet_providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider, StarknetErrorWithMessage, ProviderError, MaybeUnknownErrorCode};
 
+///
+/// Unit test for `starknet_getTransactionByHash`
+/// 
+/// purpose: call getTransactionHash on non existent transaction.
+/// fail case: transaction does not exist.
+/// 
 #[rstest]
 #[tokio::test]
 async fn fail_non_existing_transaction(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
@@ -25,6 +31,12 @@ async fn fail_non_existing_transaction(clients: HashMap<String, JsonRpcClient<Ht
     )));
 }
 
+///
+/// Unit test for `starknet_getTransactionByHash`
+/// 
+/// purpose: call getTransactionHash on INVOKE transaction.
+/// success case: retrieve correct INVOKE transaction.
+/// 
 #[rstest]
 #[tokio::test]
 async fn work_transaction_invoke(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
@@ -43,6 +55,12 @@ async fn work_transaction_invoke(clients: HashMap<String, JsonRpcClient<HttpTran
     assert_eq!(response_deoxys, response_pathfinder);
 }
 
+///
+/// Unit test for `starknet_getTransactionByHash`
+/// 
+/// purpose: call getTransactionHash on L1_HANDLER transaction.
+/// success case: retrieve correct INVOKE transaction.
+/// 
 #[rstest]
 #[tokio::test]
 async fn work_transaction_l1_handler(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
@@ -61,6 +79,14 @@ async fn work_transaction_l1_handler(clients: HashMap<String, JsonRpcClient<Http
     assert_eq!(response_deoxys, response_pathfinder);
 }
 
+// TODO: add tests for DEPLOY transaction
+
+///
+/// Unit test for `starknet_getTransactionByHash`
+/// 
+/// purpose: call getTransactionHash on DEPLOY_ACCOUNT transaction.
+/// success case: retrieve correct DEPLOY_ACCOUNT transaction.
+/// 
 #[rstest]
 #[tokio::test]
 async fn work_transaction_deploy_account(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
