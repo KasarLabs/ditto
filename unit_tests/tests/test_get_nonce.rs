9f2c46d9783acbee2679519b4c9503fe53363664
Trantorian <eliot.code@proton.me>
2024-01-09 07:19:26 +0000

    test(get_nonce): :white_check_mark: tested getNonce on non-existing contract

--- unit_tests/tests/test_get_nonce.rs
+++ unit_tests/tests/test_get_nonce.rs
@@ -4,7 +4,7 @@ mod common;
 use std::{collections::HashMap, assert_matches::assert_matches};
 
 use common::*;
-use starknet_core::types::{BlockId, FieldElement, StarknetError};
+use starknet_core::types::{BlockId, FieldElement, StarknetError, BlockTag};
 use starknet_providers::{JsonRpcClient, jsonrpc::HttpTransport, Provider, ProviderError, StarknetErrorWithMessage, MaybeUnknownErrorCode};
 
 #[rstest]
@@ -22,6 +22,25 @@ async fn fail_non_existing_block(clients: HashMap<String, JsonRpcClient<HttpTran
         Some(ProviderError::StarknetError(StarknetErrorWithMessage {
             message: _,
             code: MaybeUnknownErrorCode::Known(StarknetError::BlockNotFound)
+        }))
+    );
+}
+
+#[rstest]
+#[tokio::test]
+async fn fail_non_existing_contract(clients: HashMap<String, JsonRpcClient<HttpTransport>>) {
+    let deoxys = &clients[DEOXYS];
+
+    let response_deoxys = deoxys.get_nonce(
+        BlockId::Tag(BlockTag::Latest), 
+        FieldElement::from_hex_be(INVALID_CONTRACT_ADDR).unwrap()
+    ).await.err();
+
+    assert_matches!(
+        response_deoxys,
+        Some(ProviderError::StarknetError(StarknetErrorWithMessage {
+            message: _,
+            code: MaybeUnknownErrorCode::Known(StarknetError::ContractNotFound)
         }))
     );
 }
