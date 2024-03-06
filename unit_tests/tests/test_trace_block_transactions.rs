1b286f7ca1734e74baf9df6a64766bbbc36e47da
0xTrinityy <trinity0x@protonmail.com>
2024-03-06 18:06:51 +0100

    feat: :ambulance: Working on Ditto

---
+++ unit_tests/tests/test_trace_block_transactions.rs
@@ -0,0 +1,63 @@
+#![feature(assert_matches)]
+
+mod common;
+use common::*;
+
+use std::assert_matches::assert_matches;
+use rand::Rng;
+
+use starknet_core::types::{FieldElement, StarknetError, BlockId};
+use starknet_providers::{
+    jsonrpc::{HttpTransport, JsonRpcClient},
+    Provider, ProviderError,
+};
+
+#[rstest]
+#[tokio::test]
+async fn fail_non_existing_block(deoxys: JsonRpcClient<HttpTransport>) {
+
+    assert_matches!(
+        deoxys.trace_block_transactions(BlockId::Hash(FieldElement::ZERO)).await,
+        Err(ProviderError::StarknetError(StarknetError::BlockNotFound))
+    );
+}
+
+#[rstest]
+#[tokio::test]
+async fn works_ok_for_block_10000(deoxys: JsonRpcClient<HttpTransport>, pathfinder: JsonRpcClient<HttpTransport>) {
+
+    let block_number = BlockId::Number(10000);
+
+    let deoxys_trace = deoxys.trace_block_transactions(block_number).await;
+    let _pathfinder_trace = pathfinder.trace_block_transactions(block_number).await;
+    
+    assert_matches!(deoxys_trace, _pathfinder_trace);
+}
+
+#[rstest]
+#[tokio::test]
+async fn works_ok_for_block_300000(deoxys: JsonRpcClient<HttpTransport>, pathfinder: JsonRpcClient<HttpTransport>) {
+
+    let block_number = BlockId::Number(300000);
+
+    let deoxys_trace = deoxys.trace_block_transactions(block_number).await;
+    let _pathfinder_trace = pathfinder.trace_block_transactions(block_number).await;
+
+    assert_matches!(deoxys_trace, _pathfinder_trace);
+}
+
+#[rstest]
+#[tokio::test]
+async fn works_ok_for_random_block(deoxys: JsonRpcClient<HttpTransport>, pathfinder: JsonRpcClient<HttpTransport>) {
+    let mut rng = rand::thread_rng();
+    let random_block_number = rng.gen_range(100000..602000);
+
+    let block_number = BlockId::Number(random_block_number);
+
+    let deoxys_trace = deoxys.trace_block_transactions(block_number).await;
+    let _pathfinder_trace = pathfinder.trace_block_transactions(block_number).await;
+    println!("{:?}", deoxys_trace);
+    println!("block choose is: {:?}", block_number);
+
+    assert_matches!(deoxys_trace, _pathfinder_trace);
+}
