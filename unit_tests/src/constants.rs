e32799b32dc25be482e8781c71ac5eb292674b3e
antiyro <antiyro@gmail.com>
2024-06-17 15:46:04 +0200

    block versions

--- unit_tests/src/constants.rs
+++ unit_tests/src/constants.rs
@@ -161,7 +161,27 @@ pub const ARGENT_CONTRACT_ADDRESS: &str = "";
 pub const ERR_DEOXYS: &str = "Error waiting for response from Deoxys client";
 pub const ERR_PATHFINDER: &str = "Error waiting for response from Pathfinder client";
 
-pub const SPEC_0_5_1: &str = "0.5.1";
-pub const SPEC_0_6_0: &str = "0.6.0";
 pub const SPEC_0_7_0: &str = "0.7.0";
 pub const SPEC_0_7_1: &str = "0.7.1";
+
+///
+/// Starknet block number versions.
+/// 
+pub const BLOCK_0: u64 = 0;
+pub const BLOCK_0_9_1: u64 = 3799;
+pub const BLOCK_0_10_0: u64 = 4883;
+pub const BLOCK_0_10_1: u64 = 6570;
+pub const BLOCK_0_10_2: u64 = 12268;
+pub const BLOCK_0_10_3: u64 = 16575;
+pub const BLOCK_0_11_0: u64 = 28613;
+pub const BLOCK_0_11_0_2: u64 = 43851;
+pub const BLOCK_0_11_1: u64 = 61394;
+pub const BLOCK_0_11_2: u64 = 68096;
+pub const BLOCK_0_12_0: u64 = 103129;
+pub const BLOCK_0_12_1: u64 = 164901;
+pub const BLOCK_0_12_2: u64 = 194410;
+pub const BLOCK_0_12_3: u64 = 472644;
+pub const BLOCK_0_13_0: u64 = 501514;
+pub const BLOCK_0_13_1: u64 = 607878;
+
+
