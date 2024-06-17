d9a47dec9f25c6f90c5307bca13b74d5af21e833
antiyro <antiyro@gmail.com>
2024-06-17 17:40:47 +0200

    added transaction constants

---
+++ unit_tests/src/constants/mainnet/transaction.rs
@@ -0,0 +1,47 @@
+///
+/// Random INVOKE_V0 transaction
+/// 
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x02c02d6f0d75bc71b1c629cde038199ccfb6e18343a76943275405817727f76c)
+/// 
+pub const TX_INVOKE_V0: &str =
+    "0x02c02d6f0d75bc71b1c629cde038199ccfb6e18343a76943275405817727f76c";
+
+///
+/// Random INVOKE_V1 transaction
+///
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x027ed707907aef09c39ece1b24540308eee8d74a14c77728604c3a2da546fd6d)
+///
+pub const TX_INVOKE_V1: &str =
+    "0x027ed707907aef09c39ece1b24540308eee8d74a14c77728604c3a2da546fd6d";
+
+///
+/// Random DECLARE_V0 transaction
+/// 
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x5f430525084fe54a73e6f6de7d5d3ac20311ac4ba48002c665dade0c585af82)
+/// 
+pub const TX_DECLARE_V0: &str =
+    "0x5f430525084fe54a73e6f6de7d5d3ac20311ac4ba48002c665dade0c585af82";
+
+///
+/// Random DECLARE_V1 transaction
+/// 
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x30fd34d7ffbe2aea6121f76c7c82a9edb7250a69a86eaf4ea55e9fb39237d71)
+///
+pub const TX_DECLARE_V1: &str =
+    "0x30fd34d7ffbe2aea6121f76c7c82a9edb7250a69a86eaf4ea55e9fb39237d71";
+    
+///
+/// Random DECLARE_V2 transaction
+/// 
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x62c6e41b306db3dd03fbbb14bd169e84819e5ac1058ffde93a924b3abe9b3c1)
+/// 
+pub const TX_DECLARE_V2: &str =
+    "0x62c6e41b306db3dd03fbbb14bd169e84819e5ac1058ffde93a924b3abe9b3c1";
+
+///
+/// Random L1_HANDLER_V0 transaction
+/// 
+/// Details concerning this contract can be found on [StarkScan](https://starkscan.co/tx/0x006a555ac598673a215ef69815db1ebe89cb11eed9a489215c21c7d32d6e581a)
+/// 
+pub const TX_L1_HANDLER_V0: &str =
+    "0x006a555ac598673a215ef69815db1ebe89cb11eed9a489215c21c7d32d6e581a";
